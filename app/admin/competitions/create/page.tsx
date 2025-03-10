5ef4fddd34131c2dd863a5127c5f624132081c96
filira2004 <grobovahik11119@mail.ru>
2025-03-10 20:32:59 +0400

    start make create competitions

---
+++ app/admin/competitions/create/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Создать соревнование</div>;
+};
+
+export default Page;
--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,7 +1,6 @@
 import { LinkType } from "./definitions";
 
 export const links: LinkType[] = [
-  { name: "Домой", href: "/dashboard" },
   { name: "Соревнования", href: "/dashboard/competitions" },
   { name: "Участники", href: "/dashboard/participants" },
   { name: "Тренеры", href: "/dashboard/coaches" },
