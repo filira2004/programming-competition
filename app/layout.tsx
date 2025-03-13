4132d9f7df93a10ae9d0c704200b248824181bbc
filira2004 <grobovahik11119@mail.ru>
2025-03-13 17:42:36 +0400

    add metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,41 @@ import styles from "./layout.module.css";
 import Footer from "./ui/components/Footer/Footer";
 import { auth } from "@/auth";
 
+export const metadata = {
+  title: "PCompetitions",
+  description:
+    "PCompetitions — платформа для отслеживания результатов и информации о олимпиадах по программированию. Следите за рейтингами, расписанием и анализируйте выступления участников.",
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico",
+    other: [
+      { rel: "android-chrome-192x192", url: "/android-chrome-192x192.png" },
+      { rel: "android-chrome-512x512", url: "/android-chrome-512x512.png" },
+    ],
+  },
+};
+
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const session = await auth();
-  
+
   return (
     <html lang="ru">
       <body className={classNames(inter.className, styles.body)}>
